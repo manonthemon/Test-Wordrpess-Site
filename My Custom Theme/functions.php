0b67ac6e76637da556d8f069d6cc4c69254b6842
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-15 13:08:08 +0100

    Added menus creation to admin panel

--- My Custom Theme/functions.php
+++ My Custom Theme/functions.php
@@ -1,9 +1,13 @@
 <?php
 
-function myTestTheme_script_enqueue()
-{
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/mytesttheme.css', array(), '1.0.0', 'all');
+function myTestTheme_script_enqueue() {
+wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/mytesttheme.css', array(), '1.0.0', 'all');
+wp_enqueue_script('customjs', get_template_directory_uri(), '/js/mytesttheme.js', array(), '1.0.0', true);
 }
-wp_dequeue_script('customjs', get_template_directory_uri(), '/js/mytesttheme.js', array(), '1.0.0', true);
 
 add_action('wp_enqueue_scripts(  )', 'myTestTheme_script_enqueue');
+function myTestTheme_theme_setup(){
+add_theme_support('menus');
+}
+
+add_action('init', 'myTestTheme_theme_setup');
