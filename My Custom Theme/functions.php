c0a18807450ea239a58a9a920c635baa69795f3a
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-15 13:18:30 +0100

    Added custom menu to the top of the page

--- My Custom Theme/functions.php
+++ My Custom Theme/functions.php
@@ -7,7 +7,11 @@ wp_enqueue_script('customjs', get_template_directory_uri(), '/js/mytesttheme.js'
 
 add_action('wp_enqueue_scripts(  )', 'myTestTheme_script_enqueue');
 function myTestTheme_theme_setup(){
+
 add_theme_support('menus');
+
+register_nav_menu('primary', 'Primary Header Navigation');
+
 }
 
 add_action('init', 'myTestTheme_theme_setup');
