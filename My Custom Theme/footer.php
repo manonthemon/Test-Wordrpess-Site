4aeaa84444bb552776f5c28130b1abdfe616247f
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-15 13:22:22 +0100

    Added custom footer menu

--- My Custom Theme/footer.php
+++ My Custom Theme/footer.php
@@ -2,9 +2,7 @@
 
 <footer> 
     <p>This is my footer </p> 
-
-    
-
+    <?php wp_nav_menu(array('theme_location'=>'secondary' )); ?>
     </footer>
     <?php wp_footer(); ?>
     </body>
--- My Custom Theme/functions.php
+++ My Custom Theme/functions.php
@@ -11,7 +11,7 @@ function myTestTheme_theme_setup(){
 add_theme_support('menus');
 
 register_nav_menu('primary', 'Primary Header Navigation');
-
+register_nav_menu('secondary', 'Footer Navigation');
 }
 
 add_action('init', 'myTestTheme_theme_setup');
