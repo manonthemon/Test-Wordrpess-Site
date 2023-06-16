f91a7dd779f6f02532dc76fd92e61ff44b8e0921
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-16 13:44:19 +0100

    Added 3 pages to the website and 3 posts

--- My Custom Theme/index.php
+++ My Custom Theme/index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
-<h1>This is my test theme index</h1>
+    <?php 
+
+    if (have_posts()):
+
+    while (have_posts()): the_post(); ?>
+
+        <h2><?php the_title(); ?></h2>
+        <small>Posted on: <?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?> </small>
+        <p> <?php the_content(); ?></p>
+        <hr>
+
+    <?php endwhile;
+
+endif;
+
+?>
 
 <?php get_footer(); ?>
