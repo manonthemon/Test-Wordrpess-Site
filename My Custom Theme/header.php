a68620e8ab8e53af76f7741b075d7ae395f88756
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-16 13:58:40 +0100

    Added classes to the pages

--- My Custom Theme/header.php
+++ My Custom Theme/header.php
@@ -5,6 +5,6 @@
             <title>My Test Theme</title>
             <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 <?php wp_nav_menu(array('theme_location'=>'primary' )); ?>
