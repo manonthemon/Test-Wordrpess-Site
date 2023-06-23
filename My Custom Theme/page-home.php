5e6df18c13a1c8748898207c292786dd1dc35f8f
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-23 14:43:55 +0100

    Added posts of specific category to homepage

--- My Custom Theme/page-home.php
+++ My Custom Theme/page-home.php
@@ -52,9 +52,30 @@ if( $lastBlog->have_posts() ):
 endif;
 
 wp_reset_postdata();
-
-				
+		
 		?>
+
+<hr>
+
+<?php
+
+//PRINT ONLY TUTORIALS
+
+$lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
+
+if( $lastBlog->have_posts() ):
+        
+    while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+        
+        <?php get_template_part('content',get_post_format()); ?>
+    
+    <?php endwhile;
+    
+endif;
+
+wp_reset_postdata();
+
+?>
 	
 	</div>
 	
