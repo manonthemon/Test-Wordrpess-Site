8b1a5718f934b6f64593b8c88d53ed2c4afef180
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-26 18:07:39 +0100

    Tweeked the featured asection to only display one post from each category

--- My Custom Theme/page-home.php
+++ My Custom Theme/page-home.php
@@ -1,36 +1,45 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
 <div class="row">
-
-<?php 
-
-$args = array (
-    'type' => 'post',
-    'posts_per_page' => 3,
-    'category__in' => array(11, 12, 13)
-
-);
-
-    $lastBlog = new WP_Query($args);
-
-    if( $lastBlog->have_posts() ):
+		
+		<?php 
 			
-        while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
-
-        <div class="col-xs-12 col-sm-4">
-            
-            <?php get_template_part('content','featured'); ?>
-
-    </div>
-        
-        <?php endwhile;
-        
-    endif;
-
-    wp_reset_postdata();
-
-?>
-
+			$args_cat = array(
+				'include' => '11, 12, 13'
+			);
+			
+			$categories = get_categories($args_cat);
+			foreach($categories as $category):
+				
+				$args = array( 
+					'type' => 'post',
+					'posts_per_page' => 1,
+					'category__in' => $category->term_id,
+					'category__not_in' => array( 10 ),
+				);
+				
+				$lastBlog = new WP_Query( $args );
+				
+				if( $lastBlog->have_posts() ):
+					
+					while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+						
+						<div class="col-xs-12 col-sm-4">
+						
+							<?php get_template_part('content','featured'); ?>
+						
+						</div>
+					
+					<?php endwhile;
+					
+				endif;
+				
+				wp_reset_postdata();
+				
+			endforeach;
+		
+		?>
+		
 </div>
 
 <div class="row">
@@ -48,56 +57,54 @@ $args = array (
 			<?php endwhile;
 			
 		endif;
-
-//PRINT OTHER 2 POSTS, NOT THE FIRSt ONE
-/* $args = array(
-
-    'type' => 'post',
-    'posts_per_page' => 2,
-    'offset' => 1,
-
-);
-$lastBlog = new WP_Query($args);
-
-if( $lastBlog->have_posts() ):
-        
-    while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
-        
-        <?php get_template_part('content',get_post_format()); ?>
-    
-    <?php endwhile;
-    
-endif;
-
-wp_reset_postdata();
-
-*/
 		
-		?>
-
-<!-- <hr> -->
-
-<?php
-
-//PRINT ONLY TUTORIALS
-
-/* $lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
-
-if( $lastBlog->have_posts() ):
-        
-    while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
-        
-        <?php get_template_part('content',get_post_format()); ?>
-    
-    <?php endwhile;
-    
-endif;
-
-wp_reset_postdata();
-
+		//PRINT OTHER 2 POSTS NOT THE FIRST ONE
+/*
+		$args = array(
+			'type' => 'post',
+			'posts_per_page' => 2,
+			'offset' => 1,
+		);
+		
+		$lastBlog = new WP_Query($args);
+			
+		if( $lastBlog->have_posts() ):
+			
+			while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+		
+		wp_reset_postdata();
 */
-
-?>
+				
+		?>
+		
+		<!-- <hr> -->
+		
+		<?php
+			
+		//PRINT ONLY TUTORIALS
+/*
+		$lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
+			
+		if( $lastBlog->have_posts() ):
+			
+			while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+		
+		wp_reset_postdata();
+*/
+				
+		?>
 	
 	</div>
 	
